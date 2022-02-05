8355b4bcfdcb6da2fd411144b1f8dd67ccad39bb
upworkSolutions <98605328+upworkSolutions@users.noreply.github.com>
2022-02-05 18:16:43 +0100

    Add files via upload

---
+++ ParseText/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ParseText
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "John.Davidson/Berlin Michael.Barton/Lisbon Ivan.Perkinson/Moscow";
+            string[] names = text.Split('.', '/', ' ');
+        }
+    }
+}
